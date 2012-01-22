3a1ce116b0d06b400f27a15a9cbc2ddf35b6c2c5
Mihnea Olteanu <mihneao@gmail.com>
2012-01-22 15:53:10 -0800

    reorg some code and add a helper class - create a solution file at the root folder - add StoragePage class and unit tests

---
+++ MyRM/StoragePage.cs
@@ -0,0 +1,245 @@
+﻿namespace MyRM
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+
+    public class StoragePage
+    {
+
+        #region Constants
+
+        /// <summary>
+        /// We will use a 4 kB size page
+        /// </summary>
+        private const int PageSize = 4 * 1024;
+
+        private static readonly Encoding RecordDataEncoder = Encoding.Unicode;
+
+        #endregion
+
+        #region Private Member Variables
+
+        /// <summary>
+        /// List of records stored on the page
+        /// </summary>
+        List<byte[]> recordList;
+
+        #endregion
+
+        #region Public Methods
+
+        public StoragePage()
+        {
+            recordList = new List<byte[]>();
+        }
+
+        public int AddRecord(string data)
+        {
+            byte[] record = RecordDataEncoder.GetBytes(data);
+            if (0 > this.GetAvailableSpace() - this.GetRecordSize(record))
+            {
+                throw new InsuffcientSpaceException();
+            }
+
+            // add the data
+            this.recordList.Add(record);
+
+            // return to index
+            return this.recordList.IndexOf(record);
+        }
+
+        public void DeleteRecord(int recordIdx)
+        {
+            if (recordIdx >= this.recordList.Count
+                || 0 > recordIdx)
+            {
+                throw new InvalidRecordException();
+            }
+
+            this.recordList.RemoveAt(recordIdx);
+        }
+
+        public void WriteRecord(int recordIdx, string data)
+        {
+            if (recordIdx >= this.recordList.Count
+                || 0 > recordIdx)
+            {
+                throw new InvalidRecordException();
+            }
+
+            // encode the data
+            byte[] record = RecordDataEncoder.GetBytes(data);
+            if (0 > this.GetAvailableSpace() + this.GetRecordSize(recordIdx) - this.GetRecordSize(record))
+            {
+                throw new InsuffcientSpaceException();
+            }
+
+            this.recordList[recordIdx] = record;
+        }
+
+        public string ReadRecord(int recordIdx)
+        {
+            if (recordIdx >= this.recordList.Count
+                || 0 > recordIdx)
+            {
+                throw new InvalidRecordException();
+            }
+
+            string data = RecordDataEncoder.GetString(
+                this.recordList[recordIdx]);
+
+            return data;
+        }
+
+        public void WritePageData(FileStream stream, int pageIdx)
+        {
+            // get the page data
+            byte[] dataBuffer = new byte[PageSize];
+            this.WritePageData(dataBuffer);
+
+            // write to the file stream
+            stream.Seek(pageIdx * PageSize, SeekOrigin.Begin);
+            stream.Write(dataBuffer, 0, dataBuffer.Length);
+        }
+
+        public void ReadPageData(FileStream stream, int pageIdx)
+        {
+            byte[] dataBuffer = new byte[PageSize];
+
+            // read from the file stream
+            stream.Seek(pageIdx * PageSize, SeekOrigin.Begin);
+            stream.Read(dataBuffer, 0, dataBuffer.Length);
+
+            // initialize members
+            this.recordList = new List<byte[]>();
+            this.ReadPageData(dataBuffer);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private int GetAvailableSpace()
+        {
+            int usedSpace = 0;
+
+            // record count indicator
+            usedSpace += sizeof(int);
+
+            // record index
+            // the +1 accounts for the space that would be used for the index entry
+            // of a new record
+            usedSpace += sizeof(int) * (this.recordList.Count + 1);
+
+            // space used by each record
+            foreach (byte[] record in this.recordList)
+            {
+                usedSpace += this.GetRecordSize(record);
+            }
+
+            return (PageSize - usedSpace);
+        }
+
+        private int GetRecordSize(byte[] record)
+        {
+            return (sizeof(byte) * record.Length);
+        }
+
+        private int GetRecordSize(int recordIdx)
+        {
+            return this.GetRecordSize(this.recordList[recordIdx]);
+        }
+
+        /// <summary>
+        /// Binary Format:
+        /// |record count|record(0) size|record(0) data|record(1) size|record(1) data|...
+        /// |record(n) size|record(n) data|
+        /// </summary>
+        /// <param name="buffer"></param>
+        private void WritePageData(byte[] buffer)
+        {
+            BinaryWriter pageWriter = new BinaryWriter(
+                                        new MemoryStream(buffer));
+
+            // set the write cursor to the start of the stream
+            pageWriter.Seek(0, SeekOrigin.Begin);
+
+            // write the data
+            pageWriter.Write(this.recordList.Count);
+            foreach (byte[] record in this.recordList)
+            {
+                pageWriter.Write(record.Length);
+                pageWriter.Write(record, 0, record.Length);
+            }
+        }
+
+        /// <summary>
+        /// Binary Format:
+        /// |record count|record(0) size|record(0) data|record(1) size|record(1) data|...
+        /// |record(n) size|record(n) data| 
+        /// </summary>
+        /// <param name="buffer"></param>
+        private void ReadPageData(byte[] buffer)
+        {
+            BinaryReader pageReader = new BinaryReader(
+                                        new MemoryStream(buffer));
+
+            // set the read cursor to the start of the stream
+            pageReader.BaseStream.Seek(0, SeekOrigin.Begin);
+
+            // read the data
+            int recordCount = pageReader.ReadInt32();
+            for (int idx = 0; idx < recordCount; idx++)
+            {
+                int recordSize = pageReader.ReadInt32();
+                byte[] record = pageReader.ReadBytes(recordSize);
+
+                this.recordList.Add(record);
+            }
+        }
+
+        #endregion
+
+        #region Exception Classes
+
+        public class InvalidRecordException : System.Exception
+        {
+            public InvalidRecordException()
+                : base("Record does not exist in page.")
+            {
+            }
+
+            public InvalidRecordException(string message)
+                : base(message)
+            {
+            }
+
+            public InvalidRecordException(string message, System.Exception e)
+                : base(message, e)
+            {
+            }
+        }
+
+        public class InsuffcientSpaceException : System.Exception
+        {
+            public InsuffcientSpaceException()
+                : base("The page does not have sufficent space to store the data.")
+            {
+            }
+
+            public InsuffcientSpaceException(string message)
+                : base(message)
+            {
+            }
+
+            public InsuffcientSpaceException(string message, System.Exception e)
+                : base(message, e)
+            {
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ MyRMTests/StoragePageUnitTest.cs
@@ -0,0 +1,139 @@
+﻿
+namespace MyRMTests
+{
+    using System;
+    using System.IO;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using MyRM;
+
+
+    /// <summary>
+    /// Summary description for StoragePageUnitTest
+    /// </summary>
+    [TestClass]
+    public class StoragePageUnitTest
+    {
+        #region Constants
+
+        private const string DataFileName = "TestDataFile.txt";
+
+        #endregion
+
+        public StoragePageUnitTest()
+        {
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        [ClassInitialize]
+        public static void StoragePageUnitTestInitialize(TestContext testContext)
+        {
+            // clean up the data file
+            string fullDataFilePath = Path.Combine(testContext.TestDir, DataFileName);
+            if (File.Exists(fullDataFilePath))
+            {
+                File.Delete(fullDataFilePath);
+            }
+        }
+
+        #endregion
+
+        struct TestDataAddRecord
+        {
+            public string data;
+            public int result;
+        }
+
+        [TestMethod]
+        public void TestAddRecord()
+        {
+            TestDataAddRecord[] testDataList = 
+            {
+                new TestDataAddRecord{ data = "test data  1",   result = 0},
+                new TestDataAddRecord{ data = "test data  2",   result = 1},
+            };
+
+
+            StoragePage page = new StoragePage();
+            foreach (TestDataAddRecord testData in testDataList)
+            {
+                int result = page.AddRecord(testData.data);
+                Assert.AreEqual(testData.result, result, 
+                    string.Format("Return value did not match. Test Data=[{0}]", testData.data));
+            }
+            
+        }
+
+        struct TestDataDeleteRecord
+        {
+            public int recordIdx;
+            public string exception;
+        }
+
+        [TestMethod]
+        public void TestDeleteRecord()
+        {
+            TestDataDeleteRecord[] testDataList = 
+            {
+                new TestDataDeleteRecord{ recordIdx = -1,   exception = "InvalidRecordException"},
+                new TestDataDeleteRecord{ recordIdx = 2,    exception = "InvalidRecordException"},
+                new TestDataDeleteRecord{ recordIdx = 0,    exception = ""},
+                new TestDataDeleteRecord{ recordIdx = 0,    exception = "InvalidRecordException"},
+            };
+
+
+            StoragePage page = new StoragePage();
+            Assert.AreEqual(0, page.AddRecord("test data record"), "Adding sample record failed!");
+
+            foreach (TestDataDeleteRecord testData in testDataList)
+            {
+                try
+                {
+                    page.DeleteRecord(testData.recordIdx);
+                }
+                catch (Exception e)
+                {
+                    Assert.AreEqual(testData.exception, e.GetType().Name,
+                        string.Format("Remove Record = [{0}]", testData.recordIdx));
+                }
+            }
+
+        }
+    }
+}
