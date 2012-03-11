9e4e123372e746a1f632356619dddeab867e831d
Vincent Lung <vlung@hotmail.com>
2012-03-11 12:40:39 -0700

    Add main menu

--- CSEP545/Program.cs
+++ CSEP545/Program.cs
@@ -13,21 +13,59 @@ namespace CSEP545
         static void Main()
         {
             // THERE IS ALSO A MAIN MODULE IN Client.cs ... COMMENT THIS MAIN MODULE IF YOU ARE WORKING WITH CLIENT             
-            BasicDemo basicDemo = new BasicDemo();
-            basicDemo.ExecuteAll();
-            
-            RMDemo rmDemo = new RMDemo();
-            rmDemo.ExecuteAll();
+            bool done = false;
 
-            TPDemo tpDemo = new TPDemo();
-            tpDemo.ExecuteAll();
+            while (!done)
+            {
+                Console.Clear();
+                Console.WriteLine("=========\nMAIN MENU\n========\n1. Basic demo\n2. RM Demo\n3. TP Demo\n4. Dutch auction\n5: Console client\n6: Exit\nSelect:");
+                string input = Console.ReadLine().Trim();
 
-            DutchAuctionDemo daDemo = new DutchAuctionDemo();
-            daDemo.ExecuteAll();
-
-            // bring up the interactive client            
-            CommandLineClient client = new CommandLineClient("localhost", 8086);
-            client.ExecuteAll();
+                switch (input)
+                {
+                    case "1":
+                        {
+                            BasicDemo basicDemo = new BasicDemo();
+                            basicDemo.ExecuteAll();
+                            break;
+                        }
+                    case "2":
+                        {
+                            RMDemo rmDemo = new RMDemo();
+                            rmDemo.ExecuteAll();
+                            break;
+                        }
+                    case "3":
+                        {
+                            TPDemo tpDemo = new TPDemo();
+                            tpDemo.ExecuteAll();
+                            break;
+                        }
+                    case "4":
+                        {
+                            DutchAuctionDemo daDemo = new DutchAuctionDemo();
+                            daDemo.ExecuteAll();
+                            break;
+                        }
+                    case "5":
+                        {
+                            // bring up the interactive client            
+                            CommandLineClient client = new CommandLineClient("localhost", 8086);
+                            client.ExecuteAll();
+                            break;
+                        }
+                    case "6":
+                        {
+                            done = true;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Invalid option!");
+                            break;
+                        }
+                }
+            }
         }
     }
 }
